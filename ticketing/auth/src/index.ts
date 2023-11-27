cc9905f9063bae474b1a50e26a2c87b80d1e33f7
Yusuf Emre Beskan <yusufemre140@gmail.com>
2023-11-27 15:49:15 +0300

    added many things

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
 import { json } from "body-parser";
+import "express-async-errors";
+import { currentUserRouter } from "./routes/current-user";
+import { signOutRouter } from "./routes/signout";
+import { signInRouter } from "./routes/signin";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.get("*", async (req, res, next) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on 3000!!");
